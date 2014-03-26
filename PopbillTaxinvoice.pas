0fac17f7ffe038abb09199349146f44b24caf638
김성준 <pallet027@gmail.com>
2014-03-26 16:38:59 +0900

    add some header infomation

--- PopbillTaxinvoice.pas
+++ PopbillTaxinvoice.pas
@@ -1,3 +1,29 @@
+(*
+*=================================================================================
+* Unit for base module for Popbill API SDK. It include base functionality for
+* RESTful web service request and parse json result. It uses Linkhub module
+* to accomplish authentication APIs.
+*
+* This module uses synapse library.( http://www.ararat.cz/synapse/doku.php/ )
+* It's full open source library, free to use include commercial application.
+* If you wish to donate that, visit their site.
+* So, before using this module, you need to install synapse by user self.
+* You can refer their site or detailed infomation about installation is available
+* from below our site. We appreciate your visiting.
+*
+* For strongly secured communications, this module uses SSL/TLS with OpenSSL.
+* So You need two dlls (libeay32.dll and ssleay32.dll) from OpenSSL. You can
+* get it from Fulgan. ( http://indy.fulgan.com/SSL/ ) We recommend i386_win32 version.
+* And also, dlls must be released with your executions. That's the drawback of this
+* module, but we acommplished higher security level against that.
+*
+* http://www.popbill.com
+* Author : Kim Seongjun (pallet027@gmail.com)
+* Written : 2014-03-22
+
+* Thanks for your interest. 
+*=================================================================================
+*)
 unit PopbillTaxinvoice;
 
 interface
