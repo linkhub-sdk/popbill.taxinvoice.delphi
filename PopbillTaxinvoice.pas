54c71d045e3f4f37110dbd953c5b7bbba61335d3
1Jae <wjkim@linkhub.co.kr>
2021-06-17 09:43:48 +0900

    fixed comment updated

--- PopbillTaxinvoice.pas
+++ PopbillTaxinvoice.pas
@@ -8,7 +8,7 @@
 * Author : Kim Seongjun (pallet027@gmail.com)
 * Written : 2015-06-10
 * Contributor : Jeong Yohan (code@linkhub.co.kr)
-* Updated : 2021-04-16
+* Updated : 2021-06-15
 * Thanks for your interest.
 *=================================================================================
 *)
